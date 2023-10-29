b020daee77a8534d20cc21f84c38312a9651c255
Kunj Maheshwari <kunj.maheshwari2021@vitbhopal.ac.in>
2023-10-29 12:42:30 +0530

    Updated the backend folder and Created a new component in frontend folder

--- frontend/src/components/StudentList.js
+++ frontend/src/components/StudentList.js
@@ -1,7 +1,40 @@
+import Axios from "axios";
+import {useEffect, useState} from "react";
+import StudentListRow from "./StudentListRow";
 
 
 function StudentList(){
-    return <h1>Student List Page</h1>
+    const [arr, setArr] = useState([]);
+    useEffect(()=>{
+        Axios.get("http://localhost:4000/studentRoute/")
+        .then((res)=>{
+            if(res.status === 200)
+                setArr(res.data);
+            else
+                Promise.reject();
+        })
+        .catch((err)=> alert(err));
+    },[])
+    const ListItems = ()=>{
+        return arr.map((val, ind)=>{
+            return <StudentListRow obj={val}></StudentListRow>
+        })
+    }
+    return (
+        <table class="table table-bordered table-striped table-success">
+            <thead>
+                <tr>
+                    <th class="text-center">Name</th>
+                    <th class="text-center">Email</th>
+                    <th class="text-center">Roll Number</th>
+                    <th class="text-center">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {ListItems()}
+            </tbody>
+        </table>
+    )
 }
 
 export default StudentList;
